5f4602854eda2e5bb6444415a9329612cc365b5d
alex.musca07-cloud <musca@Katy>
2025-11-18 11:50:14 +0100

    Funciones 2

--- Reto2AlexMarcosD/src/Reto/Funciones2.java
+++ Reto2AlexMarcosD/src/Reto/Funciones2.java
@@ -1,5 +1,19 @@
 package Reto;
 
 public class Funciones2 {
+	public static double CelsiusFahrenheit(double a) {
+		return (a * 9 / 5) + 32;
+	}
 
+	public static double FahrenheitCelsius(double a) {
+		return (a - 32) * 5 / 9;
+	}
+
+	public static double LitrosGalones(double a) {
+		return a * 0.264172;
+	}
+
+	public static double GalonesLitros(double a) {
+		return a * 3.78541;
+	}
 }
