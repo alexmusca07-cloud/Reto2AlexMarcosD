4f9593e31a2742783e2c1c88df75c662a0d951dc
WiizMark <1DAM@A11-PC09>
2025-11-18 12:02:19 +0100

    Reto 2

--- Reto2AlexMarcosD/src/Reto/Reto2.java
+++ Reto2AlexMarcosD/src/Reto/Reto2.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 import Reto.Funciones1;
 import Reto.Funciones2;
+import Reto.Funciones;
 
 public class Reto2 {
 
@@ -20,12 +21,28 @@ public class Reto2 {
 		Scanner sc = new Scanner(System.in);
 
 		System.out.println("Selecciona el tipo de conversion: ");
+		System.out.println("1. Longitud");
+		System.out.println("2. Peso");
+		System.out.println("3. Temperatura");
+		System.out.println("4. Volumen");
+		System.out.println("0. Salir");
+		
 		int opcion = 0;
+		int opcionl = 0;
+		
+		
 		
 		do {
+			opcion = Funciones.dimeEntero("Dime una opcion", sc);
 			switch (opcion) {
 			case 1:
-			double n1 = Funciones1.KilometrosMillas(opcion) ;
+				do {
+					System.out.println("Selecciona a la conversión de longitud:");
+					System.out.println("1. Millas a kilómetros");
+					System.out.println("2. Kilómetros a millas");
+					System.out.println("0. Salir");
+				} while (opcionl != 0);
+			double n1 = (opcion);
 			System.out.println(n1);
 			break;
 		default:
@@ -33,7 +50,6 @@ public class Reto2 {
 			break;
 			}
 		} while (opcion != 0);
-		System.out.println("0. Salir");
 	}
 
 }
