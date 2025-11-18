b659ed9edd9f2a32610511cc320f44a7baaf0e9f
alex.musca07-cloud <musca@Katy>
2025-11-18 11:43:32 +0100

    Funciones 1

--- Reto2AlexMarcosD/src/Reto/Funciones1.java
+++ Reto2AlexMarcosD/src/Reto/Funciones1.java
@@ -1,5 +1,24 @@
 package Reto;
 
 public class Funciones1 {
+	public static double KilometrosMillas(double a) {
+		return a * 0.621371;
+	}
+	
+	public static double MillasKilometros(double a) {
+		return a * 1.60934;
+	}
+	
+	public static double KilosLibras(double a) {
+		return a * 2.20462;
+	}
+	
+	public static double LibrasKilos(double a) {
+		return a * 0.453592;
+	}
+	
+
+	
+	
 
 }
