1cf29f02a08cfaa49201da205656f3763e7122e1
WiizMark <1DAM@A11-PC09>
2025-11-18 11:50:49 +0100

    Reto 2

--- Reto2AlexMarcosD/src/Reto/Reto2.java
+++ Reto2AlexMarcosD/src/Reto/Reto2.java
@@ -1,10 +1,38 @@
 package Reto;
 
+import java.util.Scanner;
+
+import Reto.Funciones1;
+import Reto.Funciones2;
+
 public class Reto2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		/*
+		 * El programa debe mostrar un menú para que el usuario elija la operación que
+		 * desea realizar y después otro menú para elegir que quiere hacer, después se
+		 * pedirán y mostrarán los datos. Después de elegir una opción en el menú 2 y
+		 * realizar la operación, debe volver al menú 2. Para volver al menú principal
+		 * lo hará con la opción 0.
+		 */
+		
+		Scanner sc = new Scanner(System.in);
 
+		System.out.println("Selecciona el tipo de conversion: ");
+		int opcion = 0;
+		
+		do {
+			switch (opcion) {
+			case 1:
+			double n1 = Funciones1.KilometrosMillas(opcion) ;
+			System.out.println(n1);
+			break;
+		default:
+			System.out.println("0. Salir");
+			break;
+			}
+		} while (opcion != 0);
 	}
 
 }
